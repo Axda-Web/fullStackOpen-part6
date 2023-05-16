034c5ed5423b8cf073730b947dacfc5ce6d0c523
Alyx DARENNE <axda.web@gmail.com>
2023-05-16 16:27:09 +0200

    part6/ex8: Add Anecdote & AnecdoteList components + Fix bugs

---
+++ redux-anecdotes/src/components/Anecdote.jsx
@@ -0,0 +1,23 @@
+import { useDispatch } from 'react-redux'
+import { addVote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({id, content, votes}) => {
+
+    const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(addVote(id))
+  }
+  return (
+    <div key={id}>
+          <div>
+            {content}
+          </div>
+          <div>
+            has {votes}
+            <button onClick={() => vote(id)}>vote</button>
+          </div>
+        </div>
+  )
+}
+export default Anecdote
