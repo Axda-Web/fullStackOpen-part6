3a5be576249ad2bbfb9c7310ae3279c09da45f73
Alyx DARENNE <axda.web@gmail.com>
2023-05-18 07:55:41 +0200

    part6/ex20: Retrieving anecdotes from the server using React Query

--- query-anecdotes/src/App.js
+++ query-anecdotes/src/App.js
@@ -1,19 +1,24 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from 'react-query'
+import { getAnecdotes } from './requests'
 
 const App = () => {
+
+  const { data: anecdotes, isError, isLoading } = useQuery('anecdotes', getAnecdotes)
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  
+  if (isError) {
+    return <div>Annecdote service not available due to problems in server</div>
+  }
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
 
   return (
     <div>
